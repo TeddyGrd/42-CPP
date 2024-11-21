a7aca6d976c53ee9643055b45556ca8797e60120
TeddyGrd <teddyguerrand76@gmail.com>
2024-11-21 16:03:22 +0100

    correction main ex03 CPP-05

--- CPP-05/ex03/main.cpp
+++ CPP-05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tguerran <tguerran@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:21:55 by tguerran          #+#    #+#             */
-/*   Updated: 2024/11/21 15:49:39 by tguerran         ###   ########.fr       */
+/*   Updated: 2024/11/21 16:02:19 by tguerran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,12 @@
 
 int main() {
     try {
-        // Création d'un intern
         Intern intern;
 
-        // Création de bureaucrates
-        Bureaucrat highLevel("HighLevel", 1); // Peut tout signer et exécuter
-        Bureaucrat midLevel("MidLevel", 72); // Peut signer mais pas tout exécuter
-        Bureaucrat lowLevel("LowLevel", 150); // Ne peut rien faire
+        Bureaucrat highLevel("HighLevel", 1); 
+        Bureaucrat midLevel("MidLevel", 72);
+        Bureaucrat lowLevel("LowLevel", 150);
 
-        // Création de formulaires
         std::cout << "-------- Form Creation --------" << std::endl;
 
         AForm* shrubbery = intern.makeForm("shrubbery creation", "Garden");
@@ -37,45 +34,43 @@ int main() {
 
         std::cout << "-------- Signing Forms --------" << std::endl;
 
-        // Tests de signature
-        if (shrubbery) midLevel.signForm(*shrubbery); // Réussi
-        if (robotomy) midLevel.signForm(*robotomy);   // Réussi
-        if (pardon) lowLevel.signForm(*pardon);       // Échec
+        if (shrubbery) midLevel.signForm(*shrubbery); 
+        if (robotomy) midLevel.signForm(*robotomy);
+        if (pardon) lowLevel.signForm(*pardon);
 
         std::cout << "-------- Executing Forms --------" << std::endl;
 
-        // Tests d'exécution avec des grades différents
         if (shrubbery) {
-            lowLevel.executeForm(*shrubbery);   // Échec
-            midLevel.executeForm(*shrubbery);  // Réussi
+            lowLevel.executeForm(*shrubbery);
+            midLevel.executeForm(*shrubbery);
         }
 
         if (robotomy) {
-            lowLevel.executeForm(*robotomy);   // Échec
-            midLevel.executeForm(*robotomy);   // Échec
-            highLevel.executeForm(*robotomy);  // Succès ou échec (50%)
+            lowLevel.executeForm(*robotomy);
+            midLevel.executeForm(*robotomy);
+            highLevel.executeForm(*robotomy);
         }
 
         if (pardon) {
-            midLevel.executeForm(*pardon);     // Échec
-            highLevel.executeForm(*pardon);    // Réussi
+            midLevel.executeForm(*pardon);
+            highLevel.executeForm(*pardon);
         }
 
         std::cout << "-------- Final State --------" << std::endl;
 
-        // Affichage des formulaires après les tests
         if (shrubbery) std::cout << *shrubbery << std::endl;
         if (robotomy) std::cout << *robotomy << std::endl;
         if (pardon) std::cout << *pardon << std::endl;
 
         std::cout << "-------- Cleaning Up --------" << std::endl;
 
-        // Libération de mémoire
         delete shrubbery;
         delete robotomy;
         delete pardon;
 
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception& e)
+    {
         std::cerr << "Exception caught: " << e.what() << std::endl;
     }
 
